d19c0871535fe3c227ed765f5e6dee974ad7df3b
zzes <kuanglong0312@sina.com>
2017-05-14 22:50:37 +0800

    url 配置

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from article import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^$',views.home , name='home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test),
 ]
 
