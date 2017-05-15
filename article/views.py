346e65080355b25d9cd4fd83c438d7d3e4c51455
zzes <kuanglong0312@sina.com>
2017-05-15 19:53:55 +0800

    视图配置

--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
+
 # Create your views here.
 
 def home(request):
@@ -16,5 +17,24 @@ def detail(request, id):
         raise Http404
     return render(request,'post.html',{'post':post})
 
+def archieves(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request,'archieves.html',{'post_list':post_list,'error':False})
 def test(request):
     return render(request, 'test.html' , {'current_time':datetime.now()})
+
+def blog_search(request):
+    if  's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request, 'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains= s)
+            if len(post_list) == 0:
+                return render(request,'archieves.html',{'post_list':post_list,'error':True})
+            else:
+                return render(request,'archieves.html',{'post_list':post_list,'error':False})
+        return redirect('/')
